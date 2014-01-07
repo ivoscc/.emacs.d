26e7372fbb18f5007d769a58e3f504db0e9b106f
Ivo ánchez Checa Crosato <ivoscc@gmail.com>
2014-01-06 19:48:51 -0500

    Copy/Pasting goodies

--- user-lisp/setup-evil.el
+++ user-lisp/setup-evil.el
@@ -7,4 +7,12 @@
 (require 'evil-nerd-commenter)
 (evilnc-default-hotkeys)
 
+;; Make Y behave as it should.
+;; Taken from http://mattbriggs.net/blog/2012/02/27/awesome-emacs-plugins-evil-mode/
+(defun copy-to-end-of-line ()
+  (interactive)
+  (evil-yank (point) (point-at-eol)))
+
+(define-key evil-normal-state-map "Y" 'copy-to-end-of-line)
+
 (provide 'setup-evil)
