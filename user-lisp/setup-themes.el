e7238d1c96e4a5e639dee9b3d3fb39b1abf6146b
Ivo Sánchez Checa Crosato <ivoscc@gmail.com>
2014-03-11 00:18:04 -0500

    Adding tango theme

--- user-lisp/setup-themes.el
+++ user-lisp/setup-themes.el
@@ -1,6 +1,8 @@
 (require 'color-theme)
 
-(load "~/.emacs.d/themes/molokai/color-theme-molokai.el")
-(color-theme-molokai)
+;; (load "~/.emacs.d/themes/molokai/color-theme-molokai.el")
+;; (color-theme-molokai)
+(load "~/.emacs.d/themes/tangotango/color-theme-tangotango.el")
+(color-theme-tangotango)
 
 (provide 'setup-themes)
