8267e7140bb13e6e50b6bc15cd0e270504350158
Ivo Sánchez Checa Crosato <ivoscc@gmail.com>
2014-03-13 00:05:52 -0500

    Fix buggy keybinding

--- user-lisp/setup-helm.el
+++ user-lisp/setup-helm.el
@@ -4,7 +4,7 @@
       helm-ls-git-show-abs-or-relative 'relative)
 
 (global-set-key (kbd "M-p") 'helm-browse-project)
-(global-set-key (kbd "C-x M-f") 'helm-find-files)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
 (global-set-key (kbd "<f2>") 'helm-buffers-list)
 
 (provide 'setup-helm)
