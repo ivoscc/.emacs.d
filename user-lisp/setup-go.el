045c4c57f5eb5c9349e9532f9222be03fb682fbd
Ivo Sánchez Checa Crosato <ivoscc@gmail.com>
2014-03-22 12:26:59 -0500

    Using goimports instead of bare gofmt

--- user-lisp/setup-go.el
+++ user-lisp/setup-go.el
@@ -1,4 +1,8 @@
 (require 'go-mode-load)
+
+;; require goimports to be installed 
+;; go get code.google.com/p/go.tools/cmd/goimports
+(setq gofmt-command "goimports")
 (add-hook 'before-save-hook 'gofmt-before-save)
 
 (require 'auto-complete)
