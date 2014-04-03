97cea187ae7fce1b9e07bc8716178ddbbaa7d21d
Ivo Sánchez Checa Crosato <ivoscc@gmail.com>
2014-04-03 15:12:13 -0500

    Actually starting to understand how to use helm

--- user-lisp/setup-helm.el
+++ user-lisp/setup-helm.el
@@ -1,7 +1,6 @@
-(setq helm-ff-transformer-show-only-basename t 
-      helm-ls-git-show-abs-or-relative 'relative)
+(setq helm-ls-git-show-abs-or-relative 'relative
+      helm-ff-transformer-show-only-basename nil)
 
-(global-set-key (kbd "M-p") 'helm-browse-project)
+(global-set-key (kbd "M-p") 'helm-ls-git-ls)
 (global-set-key (kbd "<f2>") 'helm-buffers-list)
-
 (provide 'setup-helm)
