65a5cf3d5aed18cfe04ba9e68b545341d491d2b5
Ivo Sánchez Checa Crosato <ivoscc@gmail.com>
2014-05-15 18:54:23 -0500

    Adding some goodies copied from: - https://github.com/ppold/.emacs.d/blob/master/sane-defaults.el

---
+++ user-lisp/goodies.el
@@ -0,0 +1,24 @@
+;; Move files to trash when deleting
+(setq delete-by-moving-to-trash t)
+
+;; Transparently open compressed files
+(auto-compression-mode t)
+
+;; Answering just 'y' or 'n' will do
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+;; Remove text in active region if inserting text
+(delete-selection-mode 1)
+
+;; Always display column numbers
+(setq column-number-mode t)
+
+;; Save minibuffer history
+(savehist-mode 1)
+(setq history-length 1000)
+
+;; Don't be so stingy on the memory, we have lots now. It's the distant future.
+(setq gc-cons-threshold 20000000)
+
+(setq auto-save-default nil)
+(provide 'goodies)
