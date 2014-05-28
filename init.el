a649e3a4048c856a94bf3aae18bf96e81e32480b
Ivo Sánchez Checa Crosato <ivoscc@gmail.com>
2014-05-28 01:19:31 -0500

    Color customizations

--- init.el
+++ init.el
@@ -91,3 +91,11 @@
 (require 'auto-complete-config)
 (ac-config-default)
 (ac-flyspell-workaround)
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(speedbar-directory-face ((t (:foreground "#729fcf"))))
+ '(speedbar-tag-face ((t (:foreground "#ee4346"))))
+ '(vertical-border ((t nil))))
